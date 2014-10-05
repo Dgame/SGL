259b856183ab0e0c99397d9c1f56b5d94d25a174
Randy Schütt <rswhite4@googlemail.com>
2014-10-05 20:02:14 +0200

    Forgot to add abs for floating_point comparison...

--- include/SGL/Core/Traits.hpp
+++ include/SGL/Core/Traits.hpp
@@ -3,11 +3,12 @@
 
 #include <limits>
 #include <type_traits>
+#include <cmath>
 
 template <typename T>
 inline bool equal_floating_point(T lhs, T rhs) {
     static_assert(std::is_floating_point<T>::value, "That is not a floating point");
-    return (lhs - rhs) < std::numeric_limits<T>::epsilon();
+    return std::abs(lhs - rhs) < std::numeric_limits<T>::epsilon();
 }
 
 #endif // SGL_TRAITS_HPP
